683f98b6b60924a9801d28c31bf7b4dd0a4d75a6
Necryl <necrylthewayfarer@gmail.com>
2022-08-18 15:37:57 +0530

    define functions for add, subtract, multiply, divide in calculate object

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// defining some functions
+const calculate = {
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+};
